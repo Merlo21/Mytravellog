9a718c4cb9bd2598eae50d003d63802c234c585b
Steve <Stefano.merlini3@gmail.com>
2026-06-22 18:08:40 +0200

    fix: main with SettingsProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,24 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
+import { SettingsProvider } from "./lib/settings";
 import "./index.css";
+import Home from "./pages/Home";
+import Stats from "./pages/Stats";
+import SettingsPage from "./pages/Settings";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <SettingsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/statistiche" element={<Stats />} />
+          <Route path="/impostazioni" element={<SettingsPage />} />
+        </Routes>
+        <Toaster richColors position="top-right" />
+      </BrowserRouter>
+    </SettingsProvider>
+  </React.StrictMode>
+);
